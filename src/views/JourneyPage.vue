b3a27fa3c69a467e5022ded1d1ec39ffbf07ba39
ktyudha <ktyudha@gmail.com>
2023-10-17 17:15:45 +0700

    initial

--- src/views/JourneyPage.vue
+++ src/views/JourneyPage.vue
@@ -10,7 +10,7 @@
           <h1
             class="mb-4 text-4xl font-semibold tracking-tight leading-none text-white md:text-5xl lg:text-6xl"
           >
-            Modul & Task
+            Our Journey
           </h1>
         </div>
       </section>
